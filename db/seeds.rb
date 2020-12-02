4bde6f73df412bf7a36ac253eb949793144db732
jr1412594 <jr.medina1412@gmail.com>
2020-12-02 10:57:58 -0700

    name age and occupation on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,241 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Contestant.destroy_all
 
-Contestant.create(name:"", image:"" age: , occupation:"", likes:"" )
+Contestant.create(
+    name:"AJ", 
+    image:"", 
+    age: 28,
+    occupation:"Software Salesman",
+    likes:""
+    )
+Contestant.create(
+    name:"Ben", 
+    image:"", 
+    age: 29,
+    occupation:"Army Veteran",
+    likes:""
+    )
+Contestant.create(
+    name:"Bennett", 
+    image:"", 
+    age: 36,
+    occupation:"Wealth Management Consultant",
+    likes:""
+    )
+Contestant.create(
+    name:"Blake Monar", 
+    image:"", 
+    age: 31,
+    occupation:"Grooming Specialist",
+    likes:""
+    )
+Contestant.create(
+    name:"Blake Moynes", 
+    image:"", 
+    age: 29,
+    occupation:"Wildlife Manager",
+    likes:""
+    )
+Contestant.create(
+    name:"Brandon", 
+    image:"", 
+    age: 28,
+    occupation:"Real Estate",
+    likes:""
+    )
+Contestant.create(
+    name:"Brendon", 
+    image:"", 
+    age: 30,
+    occupation:"Commercial Roofer",
+    likes:""
+    )
+Contestant.create(
+    name:"Chasen", 
+    image:"", 
+    age: 31,
+    occupation:"IT Account Executive",
+    likes:""
+    )
+Contestant.create(
+    name:"Chris", 
+    image:"", 
+    age: 27,
+    occupation:"Landscape Design Salesman",
+    likes:""
+    )
+Contestant.create(
+    name:"Demar", 
+    image:"", 
+    age: 26,
+    occupation:"Spin Cycling Instructor",
+    likes:""
+    )
+Contestant.create(
+    name:"Eazy", 
+    image:"", 
+    age: 29,
+    occupation:"Sports Marketing Agent",
+    likes:""
+    )
+Contestant.create(
+    name:"Ed", 
+    image:"", 
+    age: 36,
+    occupation:"Health Care Salesman",
+    likes:""
+    )
+Contestant.create(
+    name:"Garin", 
+    image:"", 
+    age: 34,
+    occupation:"Professor of Journalism",
+    likes:""
+    )
+Contestant.create(
+    name:"Ivan", 
+    image:"", 
+    age: 28,
+    occupation:"Aeronautical Engineer",
+    likes:""
+    )
+Contestant.create(
+    name:"Jason", 
+    image:"", 
+    age: 31,
+    occupation:"Former Pro Football Lineman",
+    likes:""
+    )
+Contestant.create(
+    name:"Jay", 
+    image:"", 
+    age: 29,
+    occupation:"Fitness Director",
+    likes:""
+    )
+Contestant.create(
+    name:"Jeremy", 
+    image:"", 
+    age: 40,
+    occupation:"Banker",
+    likes:""
+    )
+Contestant.create(
+    name:"Joe", 
+    image:"", 
+    age: 36,
+    occupation:"Anesthesiologist",
+    likes:""
+    )
+Contestant.create(
+    name:"Jordan C.", 
+    image:"", 
+    age: 26,
+    occupation:"Software Account Executive",
+    likes:""
+    )
+Contestant.create(
+    name:"Jordan M.", 
+    image:"", 
+    age: 30,
+    occupation:"Cyber Security Engineer",
+    likes:""
+    )
+Contestant.create(
+    name:"Kenny", 
+    image:"", 
+    age: 39,
+    occupation:"Boy Band Manager",
+    likes:""
+    )
+Contestant.create(
+    name:"Mike", 
+    image:"", 
+    age: 38,
+    occupation:"Digital Media Adviser",
+    likes:""
+    )
+Contestant.create(
+    name:"Montel", 
+    image:"", 
+    age: 30,
+    occupation:"Gym Owner",
+    likes:""
+    )
+Contestant.create(
+    name:"Noah", 
+    image:"", 
+    age: 25,
+    occupation:"Registered Travel Nurse",
+    likes:""
+    )
+Contestant.create(
+    name:"Page", 
+    image:"", 
+    age: 37,
+    occupation:"Chef",
+    likes:""
+    )
+Contestant.create(
+    name:"Peter", 
+    image:"", 
+    age: 32,
+    occupation:"Real Estate Agent",
+    likes:""
+    )
+Contestant.create(
+    name:"Riley", 
+    image:"", 
+    age: 30,
+    occupation:"Attorney",
+    likes:""
+    )
+Contestant.create(
+    name:"Robby", 
+    image:"", 
+    age: 31,
+    occupation:"Insurance Broker",
+    likes:""
+    )
+Contestant.create(
+    name:"Spencer", 
+    image:"", 
+    age: 30,
+    occupation:"Water Treatment Engineer",
+    likes:""
+    )
+Contestant.create(
+    name:"Tyler C.", 
+    image:"", 
+    age: 27,
+    occupation:"Lawyer",
+    likes:""
+    )
+Contestant.create(
+    name:"Tyler S.", 
+    image:"", 
+    age: 36,
+    occupation:"Music Manager",
+    likes:""
+    )
+Contestant.create(
+    name:"Yosef", 
+    image:"", 
+    age: 30,
+    occupation:"Medical Device Salesman",
+    likes:""
+    )
+Contestant.create(
+    name:"Zac C.", 
+    image:"", 
+    age: 36,
+    occupation:"Addiction Specialist",
+    likes:""
+    )
+Contestant.create(
+    name:"Zach J.", 
+    image:"", 
+    age: 37,
+    occupation:"Clearning Serveice Owner",
+    likes:""
+    )
